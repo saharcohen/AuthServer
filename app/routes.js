2b17d3fbbfef26bf2582a982c28fe2f7565b3ed3
Sahar <sahar.chain@gmail.com>
2017-04-05 17:25:06 +0300

    after routes

---
+++ app/routes.js
@@ -0,0 +1,50 @@
+module.exports = function(app,passport){
+    // Home Page (With login links)
+    app.get('/', function(req,res){
+        res.render('index.ejs'); // load the index.ejs file
+    });
+
+    // Login Form
+    app.get('/login', function(req,res){
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', {message: req.flash('loginMessage')})
+    });
+
+    // process the login form
+    // app.post('/login',do all our passport stuff here);
+
+    // Signup page
+    // show the signup form
+
+    app.get('/signup',function(req,res){
+        //render the page and pass in any flash data if it exists
+        res.render('signup.ejs', {message: req.flash('signupMessage')})
+    });
+
+    //process the signup page
+    //app.post('/signup',do all our passport stuff here);
+
+    // Profile Section
+    // protected - have to login to see this
+    // use remote middlewaree to verify
+
+    app.get('/profile', isLoggedIn, function(req,res){
+        res.render('profile.ejs',{
+            user: req.user // get the user out of session and pass to template
+        });
+    });
+
+    // Logout
+    app.get('/logout', function(req,res){
+        req.logout();
+        res.redirect('/');
+    });
+
+};
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
--- server.js
+++ server.js
@@ -1,3 +1,41 @@
 /**
  * Created by Sahar-PC on 05/04/2017.
  */
+
+var express = require ('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database');
+
+// Configuration ====================================================================
+//mongoose.connect(configDB.url);
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console.
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view-engine','ejs'); // set up ejs for templating.
+
+app.use(session({secret: 'ilovesahar'})); // session secret
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+ // Routes ========================================
+ require('./app/routes.js');
+
+ // launch
+ app.listen(port);
+ console.log('The magic happen on port ' + port);
